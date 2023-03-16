f81c10bef53cef366b67e530beb5ce185ae90d59
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-03-16 19:36:11 +0100

    Fully working. Project finished.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // Middleware
 app.use(express.static('./public'))
@@ -11,7 +13,10 @@ app.use(express.json())
 // Routes
 app.use('/api/v1/tasks', tasks)
 
-const port = 3000
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
